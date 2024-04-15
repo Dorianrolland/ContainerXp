eaf8d152299648dd27a809405e9681824b09c996
Dorianrolland <124084062+Dorianrolland@users.noreply.github.com>
2024-04-15 17:55:10 +0200

    Update hpccm_lsim-bigdft.py

--- hpccm/hpccm_lsim-bigdft.py
+++ hpccm/hpccm_lsim-bigdft.py
@@ -18,7 +18,7 @@ from distutils.version import LooseVersion, StrictVersion
 #######
 ## Build bigdft
 #######
-image = 'nvidia/cuda:11.0-base'
+image = 'nvidia/cuda:12.4.1-runtime-ubuntu20.04'
 
 cuda_version = USERARG.get('cuda', '10.0')
 if cuda_version == "8.0":
